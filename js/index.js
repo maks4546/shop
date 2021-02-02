5c2f9d36b1c340f90b9c4b49434240fd28d14cf8
maks4546 <lukash_maksym@ukr.net>
2021-02-02 14:30:55 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
+$(function(){
+    $(".humburger").click(function(){
+        $(this).toogleClass("is-active");
 
+
+        if($(this)).hasClass('.is-active')){
+            $('#main_nav').slideDown(300);
+        }else{
+            $('#main_nav').slideUp(300);
+        }
+    });
+});
 
